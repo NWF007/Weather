00ac21783cd97418a09df79dc0cd05c87dd72295
Nico <ncfortuin27@gmail.com>
2022-06-02 09:47:09 +0200

    add data access to perticipation service

---
+++ CloudWeather.Precipitation/DataAccess/PrecipDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Precipitation.DataAccess
+{
+    public class PrecipDbContext : DbContext
+    {
+        public PrecipDbContext()
+        {
+
+        }
+
+        public PrecipDbContext(DbContextOptions opts) : base(opts)
+        {
+
+        }
+
+        public DbSet<Percipitation> Percipitations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SnakeCaseIdentityTableNames(modelBuilder);
+        }
+
+        private void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Percipitation>(b => { b.ToTable("percipitation"); });
+        }
+    }
+}
--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,4 +1,6 @@
+using CloudWeather.Precipitation.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();*/
 
-var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 /*if (app.Environment.IsDevelopment())
@@ -24,8 +25,29 @@ app.UseAuthorization();
 
 app.MapControllers();*/
 
-app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) => { 
-    return Results.Ok(zip);
+builder.Services.AddDbContext<PrecipDbContext>(
+    opts =>
+    {
+        opts.EnableSensitiveDataLogging();
+        opts.EnableDetailedErrors();
+        opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient
+    );
+
+var app = builder.Build();
+
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDbContext db) => {
+    if (days == null || days < 1 || days > 30)
+    {
+        return Results.BadRequest("Please provide a 'days' query paramenter between 1 and 30");
+    }
+
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await db.Percipitations
+        .Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate)
+        .ToListAsync();
+
+    return Results.Ok(results);
 });
 
 app.Run();
