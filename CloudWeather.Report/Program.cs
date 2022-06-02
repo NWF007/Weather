f6a010ddf9e2d3b07a3c77e07dc1fbe947d8073d
Nico <ncfortuin27@gmail.com>
2022-06-02 10:03:09 +0200

    Add data access for remaining microservices

--- CloudWeather.Report/Program.cs
+++ CloudWeather.Report/Program.cs
@@ -1,3 +1,6 @@
+using CloudWeather.Report.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();*/
+
+builder.Services.AddDbContext<WeatherReportDbContext>(
+    opts =>
+    {
+        opts.EnableSensitiveDataLogging();
+        opts.EnableDetailedErrors();
+        opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient
+    );
 
 var app = builder.Build();
 
--- CloudWeather.Temperature/Program.cs
+++ CloudWeather.Temperature/Program.cs
@@ -1,3 +1,6 @@
+using CloudWeather.Temperature.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();*/
+
+builder.Services.AddDbContext<TemperatureDbContext>(
+    opts =>
+    {
+        opts.EnableSensitiveDataLogging();
+        opts.EnableDetailedErrors();
+        opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient
+    );
 
 var app = builder.Build();
 
